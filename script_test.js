bbe82d79e3bc3746dcb52889a83a2f0f82fa55fe
Ledge <cyberboss12@users.noreply.github.com>
2025-08-28 00:42:43 +0200

    aha

--- script_test.js
+++ script_test.js
@@ -3,14 +3,14 @@ const products = [
   { img: "afbeeldingen/model.jpg", label: "Women" },
   { img: "afbeeldingen/model.jpg", label: "Children" },
   { img: "afbeeldingen/model.jpg", label: "Discover" },
-  { img: "afbeeldingen/model.jpg", label: "5" },
-  { img: "afbeeldingen/model.jpg", label: "6" },
-  { img: "afbeeldingen/model.jpg", label: "7" },
-  { img: "afbeeldingen/model.jpg", label: "8" },
-  { img: "afbeeldingen/model.jpg", label: "9" },
-  { img: "afbeeldingen/model.jpg", label: "10" },
-  { img: "afbeeldingen/model.jpg", label: "11" },
-  { img: "afbeeldingen/model.jpg", label: "12" }
+  { img: "afbeeldingen/model1.JPG", label: "5" },
+  { img: "afbeeldingen/model1.JPG", label: "6" },
+  { img: "afbeeldingen/model1.JPG", label: "7" },
+  { img: "afbeeldingen/model1.JPG", label: "8" },
+  { img: "afbeeldingen/model1.JPG", label: "9" },
+  { img: "afbeeldingen/model1.JPG", label: "10" },
+  { img: "afbeeldingen/model1.JPG", label: "11" },
+  { img: "afbeeldingen/model1.JPG", label: "12" }
 ];
 
 const grid = document.getElementById('productGrid');
