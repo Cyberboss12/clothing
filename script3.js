3c9a9c97e694496bc58eb01cc7d7ce589d63dc1b
Ledge <cyberboss12@users.noreply.github.com>
2025-09-09 21:43:54 +0200

    wissel tekst bar

---
+++ script3.js
@@ -0,0 +1,31 @@
+// Alle boodschappen die je wilt tonen
+const messages = [
+  "Welkom bij onze webshop!",
+  "Gratis verzending vanaf €50",
+  "Vandaag: 10% korting op alle tassen",
+  "Nieuw binnen: lente collectie 🌸",
+  "Sign up to recieve 15% off on your next order!",
+  "Free shipping on orders above €150",
+  "Sign up to our community newsletter!"
+];
+
+const infoMessage = document.getElementById('infoMessage');
+let msgIndex = 0;
+
+function showNextMessage() {
+  // Verberg huidige tekst
+  infoMessage.classList.add('hidden');
+
+  // Wacht tot fade-out klaar is (500ms uit CSS)
+  setTimeout(() => {
+    // Nieuwe boodschap instellen
+    msgIndex = (msgIndex + 1) % messages.length;
+    infoMessage.textContent = messages[msgIndex];
+
+    // Fade-in
+    infoMessage.classList.remove('hidden');
+  }, 500);
+}
+
+// Elke 4 seconden wisselen van boodschap
+setInterval(showNextMessage, 4000);
