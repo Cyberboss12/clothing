b2653fed7de1e7ae21464f20f29b30cc16f42465
Ledge <cyberboss12@users.noreply.github.com>
2025-09-20 18:47:09 +0200

    wijziging voor men layout

--- script2.js
+++ script2.js
@@ -1,9 +1,9 @@
-// Alle boodschappen die je wilt tonen
+// ===== Info-bar boodschappen =====
 const messages = [
   "Welkom bij onze webshop!",
   "Gratis verzending vanaf €50",
   "Vandaag: 10% korting op alle tassen",
-  "Nieuw binnen: lente collectie 🌸",
+  "Nieuw binnen: zomercollectie",
   "Sign up to recieve 15% off on your next order!",
   "Free shipping on orders above €150",
   "Sign up to our community newsletter!"
@@ -13,22 +13,17 @@ const infoMessage = document.getElementById('infoMessage');
 let msgIndex = 0;
 
 function showNextMessage() {
-  // Verberg huidige tekst
+  if (!infoMessage) return;
   infoMessage.classList.add('hidden');
 
-  // Wacht tot fade-out klaar is (500ms uit CSS)
   setTimeout(() => {
-    // Nieuwe boodschap instellen
     msgIndex = (msgIndex + 1) % messages.length;
     infoMessage.textContent = messages[msgIndex];
-
-    // Fade-in
     infoMessage.classList.remove('hidden');
-  }, 500);
+  }, 650);
 }
 
-// Elke 4 seconden wisselen van boodschap
-setInterval(showNextMessage, 4000);
+setInterval(showNextMessage, 3675);
 
 const products = [
   { img: "afbeeldingen/model.jpg", label: "Men" },
