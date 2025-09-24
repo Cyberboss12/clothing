e6d74e1944bf80e41148564124400aea462aa95e
Ledge <cyberboss12@users.noreply.github.com>
2025-09-25 00:46:25 +0200

    positionering witte balk

--- script3.js
+++ script3.js
@@ -93,7 +93,7 @@ const blackLine = document.querySelector('.black-line');
 
 if (ham && overlay && whiteBar && blackLine) {
   // kleine visuele correctie (negatief = omhoog, positief = omlaag)
-  let nudge = -18;
+  let nudge = -22;
 
   function updateBarPosition() {
     window.requestAnimationFrame(() => {
