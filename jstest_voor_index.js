aad8146ff92960763ac1db0333a887a48a68a98a
Ledge <cyberboss12@users.noreply.github.com>
2025-10-21 00:16:03 +0200

    mhm

--- jstest_voor_index.js
+++ jstest_voor_index.js
@@ -98,7 +98,7 @@ document.addEventListener("DOMContentLoaded", () => {
       grid.appendChild(createProduct(products[1]));
 
     } else {
-      for (let i = 2; i < 5; i++) {
+      for (let i = 2; i < 6; i++) {
         grid.appendChild(createProduct(products[i]));
       }
     }
